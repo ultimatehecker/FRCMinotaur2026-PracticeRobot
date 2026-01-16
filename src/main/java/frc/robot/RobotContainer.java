fa0a50f24f04d5b9bcf76abc2a52a6623ee342da
MiddletonRobotics <213078405+Middleton-Robotics@users.noreply.github.com>
2026-01-16 11:21:24 -0500

    fixed autonomous return nothing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -80,6 +80,6 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return autonomousChooser.get();
   }
 }
